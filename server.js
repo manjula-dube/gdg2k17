20c27e006816f1b4c246cf362ffc95cfcdde1e82
Vihang Patel <vihangpatel@Vihangs-MacBook-Pro.local>
2017-09-25 17:18:56 +0530

    Serve index.html using express

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+ 
+app.use(express.static(__dirname));
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.get('/', function(req, res){ 
+
+    res.sendFile('index.html');
+
+})
+
+
+var server = app.listen(PORT, function () {
+ 
+  var host = server.address().address;
+  var port = server.address().port;
+ 
+  console.log('Example app listening at http://%s:%s', host, port);
+ 
+});
